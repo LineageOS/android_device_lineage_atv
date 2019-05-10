0a2e1a0a2e0783961a6302b93379df9f85b40934
Aaron Kling <webgeek1234@gmail.com>
2019-05-09 21:18:16 -0500

    Add steam controller key layouts
    Needed to use the steam controller with the steam link app
    
    Change-Id: I1f901969d5fc68bfd5741ff7550119db92e89ecd

--- lineage_atv.mk
+++ lineage_atv.mk
@@ -13,3 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Key layouts for steam controller
+PRODUCT_PACKAGES += \
+    Vendor_28de_Product_1102.kl \
+    Vendor_28de_Product_1142.kl

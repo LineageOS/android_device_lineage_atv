ca3bb15768a14e1fa74273dfc31c703ce6dd8dec
Nolen Johnson <johnsonnolen@gmail.com>
2020-06-06 20:08:23 -0400

    atv: Import vendor/google/atv
    * Rename packages to not conflict with OpenGApps.
    
    Change-Id: Ie88a0d781b077d59bd25d2f592abb24593669199

--- lineage_atv.mk
+++ lineage_atv.mk
@@ -32,6 +32,11 @@ PRODUCT_PACKAGES += \
     Vendor_28de_Product_1102.kl \
     Vendor_28de_Product_1142.kl
 
-# TVSettings privapp permissions
+# LeanbackLauncher
+PRODUCT_PACKAGES += \
+    TVLauncherNoGMS \
+    TVRecommendationsNoGMS
+
+# priv-app permissions
 PRODUCT_COPY_FILES +=\
     device/lineage/atv/permissions/privapp-permissions-lineage-atv.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-lineage-atv.xml

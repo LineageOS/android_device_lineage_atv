23676efb926f1b4a6c64668617a0ca5deeb27ee8
Aaron Kling <webgeek1234@gmail.com>
2021-11-24 22:05:36 -0600

    Import idc and kl for Onn remote bundled with 4k stb
    Change-Id: I4235c8c91ee1e31759f9778944c1af3561624c20

--- keylayout/Android.mk
+++ keylayout/Android.mk
@@ -52,6 +52,15 @@ LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/usr/keylayout
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE        := Vendor_0957_Product_0005.kl
+LOCAL_SRC_FILES     := Vendor_0957_Product_0005.kl
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/usr/keylayout
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE        := Vendor_28de_Product_1102.kl
 LOCAL_SRC_FILES     := Vendor_28de_Product_1102.kl
 LOCAL_MODULE_TAGS   := optional

a0b1302c76b365906d1ba6c012297e34ed9ee792
Aaron Kling <webgeek1234@gmail.com>
2022-04-26 00:26:39 -0500

    Support restarting adb when adb port prop is set
    Change-Id: Ifd22b2510f4d6179a551291f2a5d685b56558af5

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -1,6 +1,13 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE               := init.lineage.atv.adb.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := init.lineage.atv.adb.rc
+LOCAL_MODULE_RELATIVE_PATH := init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE               := init.lineage.atv.resolution.rc
 LOCAL_MODULE_CLASS         := ETC
 LOCAL_SRC_FILES            := init.lineage.atv.resolution.rc

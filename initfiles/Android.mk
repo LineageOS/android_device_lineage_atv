868bf927b3e0b3050ed4c70a108d2b1b4835eb94
Nolen Johnson <johnsonnolen@gmail.com>
2022-06-24 21:11:57 -0400

    atv: Move everything to product
    * Launcher/Reccomendations go there from GMS anyway.
    * privapp perms move with the above.
    * init.lineage.atv.rc can be anywhere really.
    
    Change-Id: I26802d8afd9de39cea5b6c89688b224dfff97263

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -1,8 +1,9 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE               := init.lineage.atv.rc
 LOCAL_MODULE_CLASS         := ETC
 LOCAL_SRC_FILES            := init.lineage.atv.rc
 LOCAL_MODULE_RELATIVE_PATH := init
+LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)

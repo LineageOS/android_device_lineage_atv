42401492d9cd321a92166e27ea6374adb5cca070
Aaron Kling <webgeek1234@gmail.com>
2023-03-05 22:50:28 -0600

    Set product wide volume levels
    Change-Id: I133a4c800f092e8dee0b3b9da48093d87aee4f61

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,8 @@
 # Enable Bluetooth Remote Pairing in Setupwraith
 PRODUCT_PROPERTY_OVERRIDES += \
     atv.setup.bt_remote_pairing=true
+
+# Media volume
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.media_vol_default=20 \
+    ro.config.media_vol_steps=25

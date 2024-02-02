82a30127714425975d0d35ded070c37738075b20
Thomas Makin <halorocker89@gmail.com>
2024-02-02 13:38:25 -0500

    init: do not force 1080p scaling
    This has not been mandated by Google since S and since they fixed
    density scaling, UI looks better with properly scaling interface.
    As a backup for targets that still desire 1080p scaling, we introduce
    a new flag TARGET_ATV_FORCE_1080_SCALING to use the replacement
    initscript.
    
    Change-Id: Id1eb10b3f0c2b03ac6401bdfb00ba68036de91ef

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -3,7 +3,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE               := init.lineage.atv.rc
 LOCAL_MODULE_CLASS         := ETC
+ifneq ($(TARGET_ATV_FORCE_1080_SCALING),)
+LOCAL_SRC_FILES            := init.lineage.atv.override.rc
+else
 LOCAL_SRC_FILES            := init.lineage.atv.rc
+endif
 LOCAL_MODULE_RELATIVE_PATH := init
 LOCAL_PRODUCT_MODULE       := true
 include $(BUILD_PREBUILT)

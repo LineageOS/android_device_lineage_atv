226334f8337d3a06357a20112ba933eb78605852
Nolen Johnson <johnsonnolen@gmail.com>
2024-02-02 05:50:51 +0000

    Revert "Enable bluetooth remote pairing in Setupwraith"
    This reverts commit 076ade157fca7dbb1b1c7f13dd360be1da25fd10.
    
    Reason for revert: Property is dead, long live the RRO.
    
    Change-Id: Ia6754c31cc0208163efaf8cb4b7dbdd94ff7b8e0

--- lineage_atv.mk
+++ lineage_atv.mk
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 
-# System properties
-include $(LOCAL_PATH)/system_prop.mk
 
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += \
--- system_prop.mk
+++ system_prop.mk
@@ -1,7 +1,3 @@
-# Enable Bluetooth Remote Pairing in Setupwraith
-PRODUCT_PROPERTY_OVERRIDES += \
-    atv.setup.bt_remote_pairing=true
-
 # Media volume
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.media_vol_default=20 \

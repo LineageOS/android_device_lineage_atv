e7f731a1c6deaec9cbef0910c49cf2262c20c1f2
Aaron Kling <webgeek1234@gmail.com>
2024-02-24 23:16:31 -0600

    Disable ABI check popup
    Google added a popup dialog when 32-bit only app runs on 64-bit
    userspace. Then they ship several atv gms apps with only 32-bit native
    prebuilts. This causes several warnings during the setup wizard and when
    running the play store stating that programs are not compatible with
    this version of android. Until Google releases GMS updates that contain
    64-bit native libs, disable these warnings.
    
    Targets using 32-bit userspace would never get this popup anyways, so
    there is no need to guard this to only 64-bit targets.
    
    Change-Id: I69a5f5f535f24687ccf5f81af3fa7705ebe75090

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,7 @@
+# ABI Checks
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.wm.disable_deprecated_abi_dialog=true
+
 # Media volume
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.media_vol_default=20 \

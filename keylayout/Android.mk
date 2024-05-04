77e1e10145d12c04c5b77352e2a201fc59d9cb78
Nolen Johnson <johnsonnolen@gmail.com>
2024-05-04 00:46:13 -0400

    atv: keylayouts: Import ONN 2021 remotes
    * Several deadpool remotes also had newer revisions with
      valid changes.
    
    Change-Id: I485a1454532a1a6dcd87aa8d12e29b572df9a942

--- keylayout/Android.mk
+++ keylayout/Android.mk
@@ -203,3 +203,43 @@ LOCAL_MODULE_CLASS  := ETC
 LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/usr/keylayout
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := Vendor_0957_Product_0004.kl
+LOCAL_SRC_FILES     := Vendor_0957_Product_0004.kl
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/usr/keylayout
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := Vendor_1d5a_Product_c081.kl
+LOCAL_SRC_FILES     := Vendor_1d5a_Product_c081.kl
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/usr/keylayout
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := Vendor_1d5a_Product_c082.kl
+LOCAL_SRC_FILES     := Vendor_1d5a_Product_c082.kl
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/usr/keylayout
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := Vendor_248a_Product_8266.kl
+LOCAL_SRC_FILES     := Vendor_248a_Product_8266.kl
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/usr/keylayout
+include $(BUILD_PREBUILT)
+
+	keylayout/Vendor_1d5a_Product_c081.kl
+	keylayout/Vendor_1d5a_Product_c082.kl
+	keylayout/Vendor_248a_Product_8266.kl

8ee05b288b7db2429d1afc0b8f3f30d8b3986677
Aaron Kling <webgeek1234@gmail.com>
2024-09-20 13:46:42 -0500

    Silence most cast receiver logging
    This system is rather chatty on a working system. On a system without
    widevine keys, it's nigh unbearable. Given that we support many devkits
    and uncertified devices, purge these logs.
    
    Change-Id: Id5ed7a543e00d5c34f3d7ec0b2dd41857d2bb2f5
    (cherry picked from commit 28e18bc3e58c28252fe0bd931e9ea9afb510473f)

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,22 @@
+<<<<<<< HEAD   (f4bd58 Copy Steam Controller layouts for ble and wireless)
+=======
+# ABI Checks
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.wm.disable_deprecated_abi_dialog=true
+
+# Logging
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    log.tag.cast_ClientAuthCredsWidevine=S \
+    log.tag.cast_shell=S \
+    log.tag.cast_VolumeControlAndroid=S \
+    log.tag.katniss_interactor_CastIdFetcher=S \
+    log.tag.katniss_interactor_CastServiceInfoImpl=S \
+    log.tag.katniss_interactor_LoadCastServiceInfoTaskImpl=S \
+    log.tag.katniss_search_LoadCastServiceInfoTaskImpl=S
+endif
+
+>>>>>>> CHANGE (28e18b Silence most cast receiver logging)
 # Media volume
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.media_vol_default=20 \
